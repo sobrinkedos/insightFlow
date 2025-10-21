d0d4b05945959acd8f9ea3b039e73f350382c7d8
Rilton Oliveira de Souza <riltons@bb.com.br>
2025-10-21 00:05:53 -0300

    fix: Corrigir erro de service worker na extensao
    - Adicionar permissao contextMenus no manifest.json
    - Adicionar tratamento de erro no background.js
    - Adicionar verificacao antes de usar contextMenus
    - Adicionar logs de debug
    - Corrigir erro 'Cannot read properties of undefined'

--- browser-extension/background.js
+++ browser-extension/background.js
@@ -1,23 +1,40 @@
 // Background service worker
 
+console.log('[InsightShare] Background script loaded');
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'openPopup') {
-    chrome.action.openPopup();
+    try {
+      chrome.action.openPopup();
+    } catch (error) {
+      console.error('[InsightShare] Error opening popup:', error);
+    }
   }
 });
 
 // Set up context menu
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: 'shareWithInsightShare',
-    title: 'Compartilhar com InsightShare',
-    contexts: ['page', 'link', 'video']
-  });
+  try {
+    chrome.contextMenus.create({
+      id: 'shareWithInsightShare',
+      title: 'Compartilhar com InsightShare',
+      contexts: ['page', 'link', 'video']
+    });
+    console.log('[InsightShare] Context menu created');
+  } catch (error) {
+    console.error('[InsightShare] Error creating context menu:', error);
+  }
 });
 
 // Handle context menu clicks
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === 'shareWithInsightShare') {
-    chrome.action.openPopup();
-  }
-});
+if (chrome.contextMenus && chrome.contextMenus.onClicked) {
+  chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === 'shareWithInsightShare') {
+      try {
+        chrome.action.openPopup();
+      } catch (error) {
+        console.error('[InsightShare] Error opening popup from context menu:', error);
+      }
+    }
+  });
+}
